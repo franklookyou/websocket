6dea84d6043c74bcf64c78aab7b6e08a7f7f529e
Frank Hill <frank@arin.net>
2014-03-17 00:10:24 -0400

    Implemented zoom to cursor.  Scrolling/zooming properly bounded. Started inertial drag.

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -7,12 +7,14 @@ function init() {
     inputElement = document.getElementById( "textId" )
     outputElement = document.getElementById( "outputId" )
     containerElement = document.getElementById( "containerId" )
-    containerElement.addEventListener("mousewheel", zoom, false)
+    containerElement.addEventListener( "mousewheel", zoom, false )
     window.addEventListener( "resize", resize )
 }
 window.addEventListener( "load", init, false )
 
 var myWorker = new Worker( "/upperWorker.js"  )
+myWorker.postMessage( window.location.hostname + ":" + window.location.port )
+
 myWorker.onmessage = function( oEvent ) {
     obj = JSON.parse( oEvent.data )
 
@@ -27,6 +29,13 @@ myWorker.onmessage = function( oEvent ) {
             fill: "rgb(0,0,200)"
         })
         );
+
+        var circle = new Kinetic.Circle({
+            radius: 10,
+            fill: 'red'
+        });
+
+        draggableLayer.add( circle )
 
         draggableLayer.draw()
         writeToScreen( obj.upper )
@@ -48,55 +57,167 @@ function upperCase() {
     myWorker.postMessage( inputElement.value  )
 }
 
-
 var stage = new Kinetic.Stage({
     container: "containerId",
     width: window.innerWidth,
     height: window.innerHeight
 });
 
-var oldSize = stage.getSize()
+var prevTime = new Date( 0 )
+var prevPos = { x:0, y:0 }
+var xPerSec = 0.0
+var yPerSec = 0.0
 
-var draggableLayer = new Kinetic.Layer()
-draggableLayer.setDraggable( "draggable" )
+var clearVelocity = function() {
+    prevTime = new Date( 0 )
+    prevPos = { x:0, y:0 }
+    xPerSec = 0.0
+    yPerSec = 0.0
+}
+
+var velocityStuff = function() {
+    var curPos = draggableLayer.getPosition()
+    var curTime = new Date()
+    var unitTime = prevTime.getMilliseconds() - curTime.getMilliseconds()
+    if( unitTime > 1000 ) {
+        console.log( "clear velocity" )
+        clearVelocity()
+        return;
+    }
+
+    if( prevPos.x != 0.0 ) {
+        xPerSec = (curPos.x - prevPos.x) / (unitTime / 1000)
+    }
+
+    if( prevPos.y != 0.0 ) {
+        yPerSec = (curPos.y - prevPos.y) / (unitTime / 1000)
+    }
+
+    console.log( "velocityX:" + xPerSec )
+    console.log( "velocityY:" + yPerSec )
+
+    prevPos = curPos
+}
+
+var calculateBounds = function(pos) {
+    var oldScale = draggableLayer.getScale()
+    var backgroundPos = background.getPosition()
+    var backgroundSize = background.getSize()
+    var truncatedPos = pos
+
+
+    var upY = ( backgroundPos.y * oldScale.y ) * -1
+    if( pos.y > upY ) {
+        truncatedPos.y = upY
+    }
+
+    var leftX = ( backgroundPos.x * oldScale.x ) * -1
+    if( pos.x > leftX ) {
+        truncatedPos.x = leftX
+    }
+
+    var rightX = ( ((backgroundSize.width + backgroundPos.x) * oldScale.x) - window.innerWidth ) * -1
+    if( pos.x < rightX ) {
+        truncatedPos.x = rightX
+    }
+
+    var downY = ( ((backgroundSize.height + backgroundPos.y) * oldScale.y) - window.innerHeight ) * -1
+    if( pos.y < downY )
+    {
+        truncatedPos.y = downY
+    }
+
+    return truncatedPos
+}
+
+var draggableLayer = new Kinetic.Layer({
+    draggable: true,
+    dragBoundFunc: function(pos) {
+        velocityStuff()
+        return calculateBounds( pos )
+    }
+})
 
 //a large transparent background to make everything draggable
 var background = new Kinetic.Rect({
-    x: -1000,
-    y: -1000,
-    width: 3000,
-    height: 2000,
+    x: -2500,
+    y: -1500,
+    width: 5000,
+    height: 3000,
     fill: "#A9BA9D",
-    opacity: 1
+    opacity: 1,
+    stroke: 'olive',
+    strokeWidth: 20
 });
 
 draggableLayer.add( background )
-
 stage.add( draggableLayer )
 
 var zoom = function(e) {
     var zoomAmount = e.wheelDeltaY * 0.001
 
-    var newScale = draggableLayer.getScale()
-    var oldOffset = draggableLayer.getOffset()
+    var oldScale = draggableLayer.getScale()
+    var newScale = {
+        x: oldScale.x + zoomAmount,
+        y: oldScale.y + zoomAmount
+    }
 
-    newScale.x -= zoomAmount
-    newScale.y -= zoomAmount
+    if( (newScale.x < .35) || (newScale.x > 4) ) {
+        return
+    }
+
+    var draggablePos = draggableLayer.getPosition()
+    var mousePos = stage.getPointerPosition()
+
+    var oldMousePosition = {
+        x: ( mousePos.x - draggablePos.x ) / oldScale.x,
+        y: ( mousePos.y - draggablePos.y ) / oldScale.y
+    }
+
+//    draggableLayer.add(
+//        new Kinetic.Circle({
+//            id: "old",
+//            radius: 10,
+//            fill: 'purple',
+//            x: oldMousePosition.x,
+//            y: oldMousePosition.y
+//        })
+//    )
+
+    var newMousePosition = {
+        x: ( mousePos.x - draggablePos.x ) / newScale.x,
+        y: ( mousePos.y - draggablePos.y ) / newScale.y
+    }
+
+//    draggableLayer.add(
+//        new Kinetic.Circle({
+//            id: "new",
+//            radius: 10,
+//            fill: 'orange',
+//            x: newMousePosition.x,
+//            y: newMousePosition.y
+//        })
+//    )
+
+    var transformedPosition = {
+        x: draggablePos.x - (oldMousePosition.x - newMousePosition.x) * newScale.x,
+        y: draggablePos.y - (oldMousePosition.y - newMousePosition.y) * newScale.y
+    }
 
     draggableLayer.setScale( newScale )
-//    draggableLayer.setOffset( newOffset )
 
+    var boundedPosition = draggableLayer.dragBoundFunc()( transformedPosition )
+
+    draggableLayer.setPosition( boundedPosition )
     draggableLayer.batchDraw()
 }
 
 var resize = function(e) {
-    var newSize = draggableLayer.getSize()
-
-    newSize.width = window.innerWidth
-    newSize.height = window.innerHeight
+    var newSize = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
 
     stage.setSize( newSize )
-    oldSize = newSize
-
     stage.batchDraw()
 }
--- src/main/webapp/upperWorker.js
+++ src/main/webapp/upperWorker.js
@@ -1,9 +1,7 @@
-var wsUri = "ws://localhost:8080/upper"
-// 3/9/2014 bug prevents firefox from opening sockets on a worker
-var websocket = new WebSocket( wsUri )
+var websocket = null
 
 function consolize( obj, str ) {
-    obj.yonsole = str
+    obj.consoleMessage = str
     return obj
 }
 
@@ -11,28 +9,37 @@ function postJson( obj ) {
     this.postMessage( JSON.stringify(obj) )
 }
 
-{
-    websocket.onopen = function( evt ) {
-        var obj = consolize( {}, "CONNECTED" )
-        postJson( obj )
-    }
+var onOpen = function( evt ) {
+    var obj = consolize( {}, "CONNECTED" )
+    postJson( obj )
+}
 
-    websocket.onclose = function( evt ) {
-        var obj = consolize( {}, "CLOSED:" + evt.code + "/" + evt.reason )
-        postJson( obj )
-    }
+var onClose = function( evt ) {
+    var obj = consolize( {}, "CLOSED:" + evt.code + "/" + evt.reason )
+    postJson( obj )
+}
 
-    websocket.onerror = function( evt ) {
-        var obj = consolize( {}, 'ERROR:' + evt.code + "/" + evt.reason )
-        postJson( obj )
-    }
+var onError = function( evt ) {
+    var obj = consolize( {}, 'ERROR:' + evt.code + "/" + evt.reason )
+    postJson( obj )
+}
 
-    websocket.onmessage = function( evt ) {
-        var obj = consolize( JSON.parse(evt.data), "RECEIVED: " + evt.data )
-        postJson( obj )
-    }
+var onMessage = function( evt ) {
+    var obj = consolize( JSON.parse(evt.data), "RECEIVED: " + evt.data )
+    postJson( obj )
 }
 
 this.onmessage = function( oEvent ) {
-    websocket.send( oEvent.data )
+    if( websocket == null ) {
+        var wsUri = "ws://" + oEvent.data + "/upper"
+        websocket = new WebSocket( wsUri )
+
+        websocket.onopen = onOpen
+        websocket.onclose = onClose
+        websocket.onerror = onError
+        websocket.onmessage = onMessage
+    }
+    else {
+        websocket.send( oEvent.data )
+    }
 }
